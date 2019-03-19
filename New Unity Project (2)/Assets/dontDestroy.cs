6e6664595bce7d6d3132bea7ae6e33ba1945102e
nardo23 <leonardo.price@gmail.com>
2019-03-18 21:53:23 -0400

    music between scenes

---
+++ New Unity Project (2)/Assets/dontDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontDestroy : MonoBehaviour
+{
+   
+    void Start()
+    {
+
+        
+            DontDestroyOnLoad(this);
+
+            if (FindObjectsOfType(GetType()).Length > 1)
+            {
+                Destroy(gameObject);
+            }
+        
+    }
+    
+}
+
+
+   
---
+++ New Unity Project (2)/Assets/load_game_scene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class load_game_scene : MonoBehaviour
+{
+    public void gameScene()
+    {
+        SceneManager.LoadScene("gamescene");
+    }
+    
+}
